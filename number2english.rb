c2fc73d286b7f54f0f09095e3b95410138ebf3af
Yomi Colledge <baphled@boodah.net>
2015-06-30 10:50:52 +0100

    Drive our behaviour to handle double digit numbers

--- number2english.rb
+++ number2english.rb
@@ -1,29 +1,56 @@
 class Number2English
-	class OutOfRangeException < Exception
-	end
+  class OutOfRangeException < Exception
+  end
 
-	class Word
-		def self.hash
-			{ 
-				0 => 'zero',
-				1 => 'one'
-			}
-		end
-	end
+  class Word
+    def self.hash
+      { 
+        0 => 'zero',
+        1 => 'one',
+        2 => 'two',
+        3 => 'three',
+        4 => 'four',
+        5 => 'five',
+        6 => 'six',
+        7 => 'seven',
+        8 => 'eight',
+        9 => 'nine',
+        10 => 'ten',
+        20 => 'twenty',
+        30 => 'thirty',
+        40 => 'fourty',
+        50 => 'fifty',
+        60 => 'sixty',
+        70 => 'seventy',
+        80 => 'eighty',
+        90 => 'ninety',
+        100 => 'hundred',
+        1000 => 'thousand',
+      }
+    end
+  end
 end
 
 class Number2English
 
-	def self.parse number
-		if number < 0
-			raise Number2English::OutOfRangeException
-		end
+  def self.parse number
+    if number < 0
+      raise Number2English::OutOfRangeException
+    end
 
-		word_hash(number)
-	end
+    word_hash(number)
+  end
 
-	def self.word_hash(key)
-		hash = Word.hash
-		hash.fetch(key)
-	end
+  def self.word_hash(key)
+    hash = Word.hash
+    numbers = key.to_s.chars
+    if numbers.count == 2
+      numbers[0] = numbers[0] + '0'
+      numbers.collect do |key|
+        hash.fetch(key.to_i)
+      end.join(' ')
+    else
+      hash.fetch(key)
+    end
+  end
 end
--- spec/number2english_spec.rb
+++ spec/number2english_spec.rb
@@ -14,6 +14,28 @@ describe Number2English do
   end
 
   it 'can handle 0' do
-		expect(subject.parse(0)).to eql('zero')
-	end
+    expect(subject.parse(0)).to eql('zero')
+  end
+
+  (0..10).each do |number|
+    it "can generate a word for #{number}" do
+      expect do
+        subject.parse(number)
+      end.not_to raise_error
+    end
+  end
+
+  context 'double digit numbers' do
+    it 'can handle join double digit numbers' do
+      expect(subject.parse(22)).to eql('twenty two')
+    end
+
+    [37, 84, 53, 12, 23, 48].each do |number|
+      it "can generate a word for #{number}" do
+        expect do
+          subject.parse(number)
+        end.not_to raise_error
+      end
+    end
+  end
 end
