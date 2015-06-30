ad8b4a1006380a4e7c9ead1c4b2d76fbdbbff7ba
Yomi Colledge <baphled@boodah.net>
2015-06-30 10:07:59 +0100

    The parameter passed number should not be zero

--- number2english.rb
+++ number2english.rb
@@ -1,5 +1,12 @@
 class Number2English
+  class OutOfRangeException < Exception
+  end
+end
+
+class Number2English
   def self.parse number
-    
+    if number < 0
+      raise Number2English::OutOfRangeException
+    end
   end
 end
--- spec/number2english_spec.rb
+++ spec/number2english_spec.rb
@@ -8,4 +8,10 @@ describe Number2English do
       subject.parse(1)
     end.not_to raise_error
   end
+
+  it 'only takes a positive number' do
+    expect{ subject.parse(-1) }.to raise_error Number2English::OutOfRangeException
+  end
+
+  it 'can handle 0'
 end
